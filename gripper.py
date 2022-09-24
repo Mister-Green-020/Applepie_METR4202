9455a98bdf094b779f9c2e39db44359efa5e28e8
jdw5 <joshuadwallace1@gmail.com>
2022-09-24 20:04:21 +1000

    Added general gripper script

---
+++ gripper.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rospy
+import pigpio
+from std_msgs.msg import Bool
+
+
+def gripper_callback(open: Bool):
+    """
+    Callback function for the gripper which takes in a Boolean message from a topic re
+    """
+    if open:
+        rpi.set_servo_pulsewidth(18, 1000)
+        
+    else:
+        rpi.set_servo_pulsewidth(18, 1500)
+    
+    gripperPublisher.publish(open)
+    
+        
+
+def main() :
+    global rpi
+    global gripperPublisher
+
+    rpi = pigpio.pi()
+    rpi.set_mode(18, pigpio.OUTPUT)
+
+    rospy.init_node('gripper')
+    rospy.Subscriber('desired_gripper_position', Bool, gripper_callback)
+    
+    gripperPublisher = rospy.Publisher('gripper_position', Bool, queue_size=10)
+    rospy.Rate(10)
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
+    
