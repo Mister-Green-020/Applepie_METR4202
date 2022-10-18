d4fe29d929935831372a817d10bcabf872fd744d
Mister_Green_020 <bgreenland@y7mail.com>
2022-10-19 08:37:17 +1000

    Added pitch angle

--- constants.py
+++ constants.py
@@ -1,5 +1,9 @@
+from math import pi
+
+
 L1 = 115
 L2 = 117
 L3 = 95
 L4 = 115
+pitch_angle = -pi/3
 serial = '31702051'
--- ik_node.py
+++ ik_node.py
@@ -9,6 +9,7 @@ from sensor_msgs.msg import JointState
 from ast import increment_lineno
 from math import atan2, pi, sqrt, cos, sin
 import joint_angles
+from constants import *
 
 from mpl_toolkits import mplot3d
 
@@ -20,32 +21,40 @@ class InverseKinematics:
     def __init__(self) :
         self.pub = rospy.Publisher('desired_joint_states', JointState, queue_size=10)
         self.sub = rospy.Subscriber('new_position', Pose, self.inverse_kinematics)
-        self.pitch_angle = -60*pi/180
+        self.pitch_angle = pitch_angle
 
 
     def inverse_kinematics(self, pose: Pose) : 
         rospy.loginfo(f'Got Desired Pose:\n[\n\tpos:\n{pose.position}/\nrot:\n{pose.orientation}\n]')
         
+        # Initialise joint angles
         robot_joint_angles = joint_angles.Joint_Angles()
 
+        # Determine inverse kinematics for the robot
         robot_joint_angles.find_joint_angles(pose.position.x, pose.position.y, pose.position.z, self.pitch_angle)
 
+        # Extract joint angles from robot_joint_angles
         theta_1 = robot_joint_angles.joint_1_desired_angle
         theta_2 = robot_joint_angles.joint_2_desired_angle
         theta_3 = robot_joint_angles.joint_3_desired_angle
         theta_4 = robot_joint_angles.joint_4_desired_angle
         
+
+        # Publish desired joint states to Dynamixel Interface
+        # Create publish message
         msg = JointState(
             header = Header(stamp=rospy.Time.now()),
             name = ['joint_1', 'joint_2', 'joint_3', 'joint_4'],
             position = [theta_1, theta_2, theta_3, theta_4]
         )
 
+        # Published desired joint state message
         self.pub.publish(msg)
 
 
 
 def main(): 
+    # Debugging function
     rospy.init_node('joint_publisher', anonymous=True)
     ik_node = InverseKinematics()
     rospy.spin() 
