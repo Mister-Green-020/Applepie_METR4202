338504ff27c7c1de31dbd5e8ba40affaf13e2fe7
Mister_Green_020 <bgreenland@y7mail.com>
2022-10-21 17:10:05 +1000

    added camera frame dims and changed link lengths

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,11 @@
 from math import pi
 
 #unit in mm
-L1 = 115
-L2 = 117
+L1 = 100
+L2 = 117.5
 L3 = 95
-L4 = 115
+L4 = 91.46
+
 #pitch angle is fixed in -60 degree
 pitch_angle = -pi/3
 serial = '31702051'
@@ -12,4 +13,6 @@ serial = '31702051'
 # Camera location relative robot base
 camera_x = 190
 camera_y = 0
-camera_z = 450
+camera_z = 400
+camera_height = 640
+camera_width = 512
