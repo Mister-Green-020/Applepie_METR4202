8765412bd9fe5b179db28bdc774e7d74f53194ee
Mister_Green_020 <bgreenland@y7mail.com>
2022-10-22 16:17:02 +1000

    the circle of life

--- src/the_revenge_of_miguel.py
+++ src/the_revenge_of_miguel.py
@@ -29,15 +29,11 @@ class CameraViewer:
 
     (rows,cols,channels) = cv_image.shape
     if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
+      cv2.circle(cv_image, (int(cam_h/2),int(cam_w/2)), 30, 255)
 
     cv2.imshow("Image window", cv_image)
     cv2.waitKey(3)
 
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
 
 
 
