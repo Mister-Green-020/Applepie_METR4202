7d092eb158c3963b2ffeb3def30643fa37443b4c
jdw5 <joshuadwallace1@gmail.com>
2022-10-22 16:10:55 +1000

    Testing new CVBridge

--- src/the_revenge_of_miguel.py
+++ src/the_revenge_of_miguel.py
@@ -22,47 +22,33 @@ class CameraViewer:
     self.test_pub = rospy.Publisher("/cd_test", Image, queue_size=10)
 
   def callback(self, data):
-    global img
-
-    # rospy.loginfo(len(data.data))
-    data.data = data.data[int(3 * cam_h * cam_w / 2) : int(3 * cam_h * cam_w/2 + (3*20))]
-    data.height = 20
-    data.width = 1
-
-    data.step = 3
-    print(len(data.data))
-    # rospy.loginfo(f"{data.height}")
-    # rospy.loginfo(f"{data.width}")
-    self.test_pub.publish(data)
-    
     try:
-      img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
-    
+
+    (rows,cols,channels) = cv_image.shape
+    if cols > 60 and rows > 60 :
+      cv2.circle(cv_image, (50,50), 10, 255)
+
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
+
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
 
 
-    bgr = img[img.shape[0] // 2, img.shape[1] // 2, :]
-    color = ColorRGBA()
-    color.r = bgr[2]
-    color.g = bgr[1]
-    color.b = bgr[0]
-    viewer.color_pub.publish(color)
-    # msg = String(
-    #   data=f'{len(data.data)}'
-    # )
-    # self.test_pub.publish(msg)
 
-
-
-if __name__ == '__main__':  
+def main():
   rospy.init_node('image_node', anonymous=True)
   viewer = CameraViewer(SERIAL)
   try:
-    while not rospy.is_shutdown():
-        if img is not None:
-            cv2.imshow("Image", img)
-            cv2.waitKey(1)
+    rospy.spin()
   except KeyboardInterrupt:
     print("Shutting down")
   cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
